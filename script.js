625ac48d1bfe41262ef2c2bb5e41430c2384cce1
Caleb Ruhm <ruhmc0619@gmail.com>
2021-08-17 20:36:53 -0400

    jsupdate2

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function sConsole(event) {
     event.preventDefault();
     var data = document.getElementById("data");
     console.log(data.value);
+    document.getElementById('data').value=null;
+    alert("Thanks!");
 }
